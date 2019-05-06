e5a60cb37982fb2de5745f99301189460c5f86e7
Mariana Sandoval <mariana.sandoval@unosquare.com>
2019-05-06 16:57:14 -0500

    Exercise 3.1 - Development c#

---
+++ ex3_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml;
+
+namespace ex3_1
+{
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+            ReadFile();
+        }
+        public static bool  ReadFile()
+        {
+            XmlTextReader xtr = new XmlTextReader("XmlFile.xml");
+                while (xtr.Read())
+                { 
+                    if (xtr.Name == "name")
+                    {
+                    var product = xtr.ReadElementString();
+                       //price = xmlTextReader.
+                        Console.WriteLine("PRODUCT NAME:{0}  ",product);
+                    }
+                    if (xtr.Name == "price")
+                    {
+                    var price = xtr.ReadElementString();
+                    //price = xmlTextReader.
+                    Console.WriteLine("PRICE: {0} \n",price);
+                    }
+                }
+            return false;
+        }
+
+    }
+}
